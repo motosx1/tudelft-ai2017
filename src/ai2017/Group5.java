885320386a269de6764f146c330765cc5c6b26a8
Bartosz Z <motosx1@gmail.com>
2017-10-17 12:20:21 +0200

    features permutations, but sths wrong

--- src/ai2017/Group5.java
+++ src/ai2017/Group5.java
@@ -2,30 +2,34 @@ package src.ai2017;
 
 import negotiator.AgentID;
 import negotiator.Bid;
+import negotiator.DomainImpl;
 import negotiator.actions.Accept;
 import negotiator.actions.Action;
 import negotiator.actions.Offer;
+import negotiator.issue.Issue;
+import negotiator.issue.IssueDiscrete;
 import negotiator.issue.ValueDiscrete;
 import negotiator.parties.AbstractNegotiationParty;
 import negotiator.parties.NegotiationInfo;
 import negotiator.utility.EvaluatorDiscrete;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
 
 /**
  * This is your negotiation party.
  */
 public class Group5 extends AbstractNegotiationParty {
-
+    private Random rand = new Random(200);
     private Bid lastReceivedBid = null;
+    private int step = 1;
 
     private Map<String, EvaluatorDiscrete> dummyUtilitySpace = new HashMap<>(); // == fevaluators
 
-    private Map<Map<Map.Entry<String, EvaluatorDiscrete>, Double>, Double> hSpace = new HashMap<>();
-    ;
+    private Map<Map<Map.Entry<String, List<Map<ValueDiscrete, Double>>>, Double>, Double> hSpace = new HashMap<Map<Map.Entry<String, List<Map<ValueDiscrete, Double>>>, Double>, Double>();
+
+    double a = 0.5;//0.2 + Math.sin(Math.PI / 6);
 
     @Override
     public void init(NegotiationInfo info) {
@@ -41,28 +45,150 @@ public class Group5 extends AbstractNegotiationParty {
         prepareDummy();
 
         List<Map.Entry<String, EvaluatorDiscrete>> utilitySpaceList = new ArrayList<>(dummyUtilitySpace.entrySet());
+        Map<String, List<Map<ValueDiscrete, Double>>> featuresPermutationsMap = new HashMap<>();
+        for (Map.Entry<String, EvaluatorDiscrete> entry : dummyUtilitySpace.entrySet()) {
+            Set<ValueDiscrete> features = entry.getValue().getValues();
+            List<ValueDiscrete> featuresList = new ArrayList<>(features);
+            List<List<ValueDiscrete>> featuresPermutations = SetPermutations.getSetPermutations(featuresList);
+            List<Map<ValueDiscrete, Double>> featuresPermutationsWithWeights = new ArrayList<>();
 
-        double a = 0.5;
+            // assign weights
 
-        List<List<Map.Entry<String, EvaluatorDiscrete>>> setPermutations = SetPermutations.getSetPermutations(utilitySpaceList);
 
-        for (List<Map.Entry<String, EvaluatorDiscrete>> setPermutation : setPermutations) {
-//            System.out.println(setPermutation);
-            Map<Map.Entry<String, EvaluatorDiscrete>, Double> hElem = new HashMap<>();
+            for (List<ValueDiscrete> permutation : featuresPermutations) {
+                Map<ValueDiscrete, Double> featureWeightMap = new HashMap<>();
+                int n = featuresPermutations.size();
+                double sn = (1 - Math.pow(a, n)) / (1 - a);
+                double cwn = 1 / sn;
+
+                for (ValueDiscrete feature : permutation) {
+                    featureWeightMap.put(feature, cwn);
+                    cwn = cwn * a;
+                }
+
+                featuresPermutationsWithWeights.add(featureWeightMap);
+
+
+//                System.out.println('x');
+            }
+
+
+            featuresPermutationsMap.put(entry.getKey(), featuresPermutationsWithWeights);
+
+        }
+
+        Set<Map.Entry<String, List<Map<ValueDiscrete, Double>>>> featuresPermutationsMapEntries = featuresPermutationsMap.entrySet();
+        List<Map.Entry<String, List<Map<ValueDiscrete, Double>>>> entries = new ArrayList<>(featuresPermutationsMapEntries);
+        List<List<Map.Entry<String, List<Map<ValueDiscrete, Double>>>>> setPermutations = SetPermutations.getSetPermutations(entries);
+
+        System.out.println(featuresPermutationsMap);
+
+        for (Map.Entry<String, List<Map<ValueDiscrete, Double>>> entry1 : featuresPermutationsMap.entrySet()) {
+            for (Map<ValueDiscrete, Double> entry2 : entry1.getValue()) {
+                for (Map.Entry<ValueDiscrete, Double> entry3 : entry2.entrySet()) {
+                    System.out.println(entry1.getKey() + " -> " + entry3.getKey() + " -> " + entry3.getValue());
+                }
+            }
+        }
+
+
+
+
+
+        for (List<Map.Entry<String, List<Map<ValueDiscrete, Double>>>> setPermutation : setPermutations) {
+            Map<Map.Entry<String, List<Map<ValueDiscrete, Double>>>, Double> hElem = new HashMap<>();
             int n = setPermutation.size();
             double sn = (1 - Math.pow(a, n)) / (1 - a);
             double cwn = 1 / sn;
-            for (Map.Entry<String, EvaluatorDiscrete> entry1 : setPermutation) {
+            for (Map.Entry<String, List<Map<ValueDiscrete, Double>>> entry1 : setPermutation) {
                 hElem.put(entry1, cwn);
-//                System.out.println(entry1 + " " + entry1.getValue().getWeight() + " | " + "w = " + cwn);
                 cwn = cwn * a;
             }
             hSpace.put(hElem, 1 / (double) setPermutations.size());
-            System.out.println("-----------");
         }
+
 
         System.out.println("end");
 
+        try {
+            fight();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void fight() throws IOException {
+        Bid oppBid = generateDummyBid();
+
+//        System.out.println(Arrays.toString(hSpace.entrySet().toArray()));
+
+
+//        for (Map.Entry<Map<Map.Entry<String, List<Map<ValueDiscrete, Double>>>, Double>, Double> hSpaceEntry : hSpace.entrySet()) {
+//            System.out.println(hSpaceEntry.getKey());
+//            for (Map.Entry<Map.Entry<String, List<Map<ValueDiscrete, Double>>>, Double> entry1 : hSpaceEntry.getKey().entrySet()) {
+//                System.out.println("\t" + entry1);
+//                for (Map<ValueDiscrete, Double> entry2 : entry1.getKey().getValue()) {
+//                    System.out.println("\t\t" + entry2);
+//
+//                    for (Map.Entry<ValueDiscrete, Double> entry3 : entry2.entrySet()) {
+////                        System.out.println("->" + hSpaceEntry + "(" + entry1.getValue() + "){" + entry3.getKey() + "(" + entry3.getValue() + ")" + "}");
+//                    }
+//
+//
+//                }
+//            }
+//        }
+
+//        System.out.println("end");
+
+//        for (Map.Entry<Map<Map.Entry<String, List<Map<ValueDiscrete, Double>>>, Double>, Double> hSpaceEntry : hSpace.entrySet()) {
+//
+//            double newPhb = calculatePhb(oppBid, hSpaceEntry);
+//        }
+
+    }
+
+    private double calculatePhb(Bid oppBid, Map.Entry<Map<Map.Entry<String, List<Map<ValueDiscrete, Double>>>, Double>, Double> hSpaceEntry) {
+        Map<Map.Entry<String, List<Map<ValueDiscrete, Double>>>, Double> hj = hSpaceEntry.getKey();
+        Double phj = hSpaceEntry.getValue();
+
+        double numerator = phj * calculatePbh();
+        double denominator = 1;
+
+        return numerator / denominator;
+    }
+
+    private double calculatePbh() {
+        double sigma = 0.4;
+        double c = 1 / (sigma * Math.sqrt(2 * Math.PI));
+
+        double index = Math.pow(utilityBgivenH() - utilityB(), 2) / (2 * Math.pow(sigma, 2));
+
+        return c * Math.exp(index);
+    }
+
+    private double utilityBgivenH() {
+        return 1;
+    }
+
+    private double utilityB() {
+        return 1 - 0.05 * step++;  //TODO change to consider time horizon
+    }
+
+
+    private Bid generateDummyBid() throws IOException {
+        DomainImpl domain = new DomainImpl(new File("etc/templates/partydomain/simple/party_domain.xml"));
+        HashMap values = new HashMap();
+
+        for (Issue issue : domain.getIssues()) {
+            IssueDiscrete discreteIssue = (IssueDiscrete) issue;
+            int index = 0;//rand.nextInt(discreteIssue.getNumberOfValues());
+            ValueDiscrete value = discreteIssue.getValue(index);
+            values.put(issue.getNumber(), value);
+
+        }
+
+        return new Bid(domain, values);
     }
 
     private void prepareDummy() {
@@ -77,14 +203,14 @@ public class Group5 extends AbstractNegotiationParty {
             eval = new EvaluatorDiscrete();
             eval.setWeight(0.1);
             eval.setEvaluationDouble(new ValueDiscrete("Party Room"), 0.8);
-            eval.setEvaluationDouble(new ValueDiscrete("Your dorm"), 0.2);
+            eval.setEvaluationDouble(new ValueDiscrete("Your Dorm"), 0.2);
             dummyUtilitySpace.put("Location", eval);
 
-            eval = new EvaluatorDiscrete();
-            eval.setWeight(0.4);
-            eval.setEvaluationDouble(new ValueDiscrete("Beer"), 0.8);
-            eval.setEvaluationDouble(new ValueDiscrete("Wine"), 0.2);
-            dummyUtilitySpace.put("Drinks", eval);
+//            eval = new EvaluatorDiscrete();
+//            eval.setWeight(0.4);
+//            eval.setEvaluationDouble(new ValueDiscrete("Beer"), 0.8);
+//            eval.setEvaluationDouble(new ValueDiscrete("Wine"), 0.2);
+//            dummyUtilitySpace.put("Drinks", eval);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -103,7 +229,7 @@ public class Group5 extends AbstractNegotiationParty {
 
         // with 50% chance, counter offer
         // if we are the first party, also offer.
-        if (lastReceivedBid == null || !validActions.contains(Accept.class) || Math.random() > 0.5) {
+        if (lastReceivedBid == null || !validActions.contains(Accept.class) || Math.random() > 0.1) {
             return new Offer(getPartyId(), generateRandomBid());
         } else {
             return new Accept(getPartyId(), lastReceivedBid);
